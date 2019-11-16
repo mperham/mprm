317b5391e99cf060662f15db50e7d72a677d4fa4
Mike Perham <mperham@gmail.com>
2019-11-16 11:39:44 -0800

    Fix default passphrase value of "false"

--- lib/mprm/debian.rb
+++ lib/mprm/debian.rb
@@ -200,7 +200,7 @@ module Debian
 
       if gpg.nil?
         sign_cmd = "gpg --digest-algo \"#{sign_algorithm}\" --yes --output Release.gpg -b Release"
-      elsif !gpg_passphrase.nil?
+      elsif gpg_passphrase
         sign_cmd = "echo \'#{gpg_passphrase}\' | gpg --digest-algo \"#{sign_algorithm}\" -u #{gpg} --passphrase-fd 0 --yes --output Release.gpg -b Release"
       else
         sign_cmd = "gpg --digest-algo \"#{sign_algorithm}\" -u #{gpg} --yes --output Release.gpg -b Release"
